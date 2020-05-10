2bc7ff153b8ad8696863ed9e3492d65869a94525
gaston-duault <61382435+gaston-duault@users.noreply.github.com>
2020-05-10 14:42:18 +0200

    maj 10/05/2020

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const burger = document.querySelector('.burger');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+});
+
+
+window.onscroll = function() { myFunction() };
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
